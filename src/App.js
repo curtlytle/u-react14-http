9baff43920dd532fe937b2f5e24d9b77bc59133b
Curt Lytle <Curt.Lytle@idahosif.org>
2021-05-06 11:41:25 -0600

    Handling errors

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function App() {
         setError(null);
         try {
             const response = await fetch('https://swapi.dev/api/films/');
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
             const data = await response.json();
 
             const transformedMovies = data.results.map(movieData => {
@@ -24,10 +27,23 @@ function App() {
                 }
             });
             setMovies(transformedMovies);
-            setIsLoading(false);
         } catch (error) {
-
+            setError(error.message);
         }
+        setIsLoading(false);
+    }
+
+    let content = <p>Found no movies.</p>;
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies}/>;
+    }
+
+    if (error) {
+        content = <p>{error}</p>;
+    }
+    if (isLoading) {
+        content = <p>Loading...</p>;
     }
 
     return (
@@ -36,9 +52,7 @@ function App() {
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
             <section>
-                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
-                {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-                {isLoading && <p>Loading...</p>}
+                {content}
             </section>
         </React.Fragment>
     );
